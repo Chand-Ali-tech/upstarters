c1caac1909c351952919873813cace08ef09675f
Chand Ali <139934421+Chand-Ali-tech@users.noreply.github.com>
2025-06-20 14:14:14 +0500

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -20,7 +20,7 @@ const CreateSendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_In * 24 * 60 * 60 * 1000
     ),
-    // httpOnly: true,
+    httpOnly: true,
     sameSite: "None"
   };
 
